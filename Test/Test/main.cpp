20568675b32018fc27a55399b06f5ad38c1fd3ec
zeroinfinity <gianni.codevico@zeroinfinity.it>
2014-02-04 15:12:41 +0100

    PreTest
    Solito Pre Test

---
+++ Test/Test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Test
+//
+//  Created by gianni on 04/02/14.
+//  Copyright (c) 2014 gianni. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
